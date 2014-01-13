ba7981354354e4884c21552118c2b1de89aa40f9
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-01-13 11:22:55 +0100

    osname back to 'android' but after Configure

--- hints/linux-android.sh
+++ hints/linux-android.sh
@@ -21,12 +21,29 @@ unset vendorman1
 unset vendorman3
 installusrbinperl='undef'
 i_locale='undef'
+d_locconv='undef'
 d_setlocale='undef'
 d_setlocale_r='undef'
 d_getservent_r='undef'
 d_getpwnam_r='undef'
 d_getpwuid_r='undef'
 d_getpwent='undef'
+d_useshellcmds='define'
+d_libname_unique='define'
 sh='/system/bin/sh'
+libswanted="$libswanted m"
 libs='-ldl -lm -lc'
 libc='/system/lib/libc.so'
+
+# Hackery. We want osname to be linux-android during Configure,
+# but plain 'android' afterwards.
+case "$src" in
+    /*) pwd="$src";;
+    *)  pwd=`test -f ../Configure && cd ..; pwd`
+        ;;
+esac
+
+$cat <<EOO >> $pwd/config.arch
+
+osname='android'
+EOO
