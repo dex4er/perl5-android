ea0ef416a32a063d3922d4906ed6e32c7dca6bbc
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-01-20 17:19:26 +0100

    emscripten target

---
+++ hints/emscripten.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+osname='emscripten'
+osvers='3.2'
+archname='llvm-llvm-emscripten'
+
+run='nodejs'
+exe_ext='.js'
+
+cc='emcc'
+d_cplusplus='undef'
+ccflags="$ccflags"
+
+ld='emcc'
+ldflags="$ldflags -Wno-warn-absolute-paths -s DLOPEN_SUPPORT=1 -s MAIN_MODULE=1 -s ASM_JS=1 -O1"
+perllibs=''
+cryptlib=''
+
+d_procselfexe='undef'
+#d_libm_lib_version='undef'
+#useperlio='undef'
